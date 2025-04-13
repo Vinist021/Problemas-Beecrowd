7e91058c081a1f5a9b7938ecd04e4d0acb02e242
Vinicius Teixeira <vinist.002@gmail.com>
2025-04-13 14:17:35 -0300

    Produto Simples

---
+++ Todos/C/BEE 1004.c
@@ -0,0 +1,21 @@
+/*
+  autor: Vinist021;
+  data: 21/08/2024;
+  nome: Produto Simples;
+*/
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	
+	int A, B, prod;
+	
+	scanf("%d", &A);
+	scanf("%d", &B);
+	
+	prod = A * B;
+	
+	printf("PROD = %d\n", prod);
+	
+	return 0;
+}
