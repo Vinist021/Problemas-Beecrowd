29d5d88cc38b19f457515f6bf5304391c4bcf67c
Vinicius Teixeira <vinist.002@gmail.com>
2025-04-13 14:15:57 -0300

    Soma Simples

---
+++ Todos/C/BEE 1003.c
@@ -0,0 +1,21 @@
+/*
+  autor: Vinist021;
+  data: 21/08/2024;
+  nome: Soma Simples;
+*/
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	
+	int A, B, soma;
+	
+	scanf("%d", &A);
+	scanf("%d", &B);
+	
+	soma = A + B;
+	
+	printf("SOMA = %d\n", soma);
+	
+	return 0;
+}
