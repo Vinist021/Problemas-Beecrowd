4117e5cfbc6e279bd51caf26a28ce995a384ef76
Vinicius Teixeira <vinist.002@gmail.com>
2025-04-13 14:11:50 -0300

    Extremamente Básico

---
+++ Todos/C/BEE 1001.c
@@ -0,0 +1,21 @@
+/*
+  autor: Vinist021;
+  data: 21/08/2024;
+  nome: Extremamente Básico;
+*/
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	
+	int A, B, soma;
+	
+	scanf("%d", &A);
+	scanf("%d", &B);
+
+	soma = A + B;
+	
+	printf("X = %d\n", soma);
+		
+	return 0;
+}
