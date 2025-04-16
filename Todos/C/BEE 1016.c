2fa4627f345737d50f69863acd74a2fce426ea1f
Vinicius Teixeira <vinist.002@gmail.com>
2025-04-16 06:58:00 -0300

    Distância

---
+++ Todos/C/BEE 1016.c
@@ -0,0 +1,20 @@
+/*
+  autor: Vinist021;
+  data: 27/08/2024;
+  nome: Distância;
+*/
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	
+	int dist, tempo;
+	
+	scanf("%d", &dist);
+	
+	tempo = dist * 2;
+	
+	printf("%d minutos\n", tempo);
+	
+	return 0;
+}
