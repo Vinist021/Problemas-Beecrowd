de4e9d289c85466c3ac6283d49b742aa74c831b0
Vinicius Teixeira <vinist.002@gmail.com>
2025-04-18 14:03:00 -0300

    Coordenadas de um Ponto

---
+++ Todos/C/BEE 1041.c
@@ -0,0 +1,31 @@
+/*
+  autor: Vinist021;
+  data: 06/11/2024;
+  nome: Coordenadas de um Ponto;
+*/
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	
+	float X, Y;
+	
+	scanf("%f %f", &X, &Y);
+	
+	if(X == 0 & Y == 0){
+		printf("Origem\n");
+	}	else if(X == 0 && Y != 0){
+			printf("Eixo Y\n");
+		}	else if(X != 0 && Y == 0){
+				printf("Eixo X\n");
+			}	else if(X > 0 && Y > 0){
+					printf("Q1\n");
+				} 	else if(X < 0 && Y > 0){
+						printf("Q2\n");
+					}	else if(X < 0 && Y < 0){
+							printf("Q3\n");
+						}	else if(X > 0 && Y < 0){
+								printf("Q4\n");
+							}															                
+	return 0;
+}
