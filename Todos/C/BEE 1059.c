e219cfd93636e0201abdfe7910f1d0d6245dbfa7
Vinicius Teixeira <vinist.002@gmail.com>
2025-04-21 09:58:46 -0300

    Números Pares

---
+++ Todos/C/BEE 1059.c
@@ -0,0 +1,16 @@
+/*
+  autor: Vinist021;
+  data: 04/09/2024;
+  nome: Números Pares;
+*/
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	
+	for(int i=2; i<=100; i+=2){
+		printf("%d\n", i);
+	}
+	
+	return 0;
+}
