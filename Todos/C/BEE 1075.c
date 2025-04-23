25502612c79470ea67d3c5467d7d94383f49bfc7
Vinicius Teixeira <vinist.002@gmail.com>
2025-04-23 15:00:18 -0300

    Resto 2

---
+++ Todos/C/BEE 1075.c
@@ -0,0 +1,26 @@
+/*
+  autor: Vinist021;
+  data: 05/09/2024;
+  nome: Resto 2;
+*/
+
+#include <stdio.h>
+
+#define num 10000
+
+int main(int argc, char *argv[]) {
+	
+	int N;
+	
+	scanf("%d", &N);
+	
+	for(int i = 1; i <= num; i++)
+	{
+		if(i % N == 2)
+		{
+			printf("%d\n", i);
+		}
+	}
+	
+	return 0;
+}
