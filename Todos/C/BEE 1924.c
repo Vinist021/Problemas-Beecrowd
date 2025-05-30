4c4d8568c9f650781877c661bb03f52e5d8eaef1
Vinicius Teixeira <vinist.002@gmail.com>
2025-05-30 20:19:53 -0300

    Vitória e a Indecisão

---
+++ Todos/C/BEE 1924.c
@@ -0,0 +1,27 @@
+/*
+  autor: Vinist021;
+  data: 27/11/2024;
+  nome: Vitória e a Indecisão;
+*/
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	
+	char S[101];
+	int n;
+	
+	scanf("%d", &n);
+	
+	int i = 0;
+	while(i < n)
+	{
+		scanf(" %s", S);
+		
+		i++;
+	}
+	
+	printf("Ciencia da Computacao\n");
+	
+	return 0;
+}
