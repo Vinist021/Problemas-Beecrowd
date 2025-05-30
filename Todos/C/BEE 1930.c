dc4334886d5bb39e7ee8ffed56648b63ba290aa9
Vinicius Teixeira <vinist.002@gmail.com>
2025-05-30 20:28:56 -0300

    Tomadas

---
+++ Todos/C/BEE 1930.c
@@ -0,0 +1,18 @@
+/*
+  autor: Vinist021;
+  data: 30/11/2024;
+  nome: Tomadas;
+*/
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	
+	int n1, n2, n3, n4;
+	
+	scanf("%d %d %d %d", &n1, &n2, &n3, &n4);
+	
+	printf("%d\n", (n1 + n2 + n3 + n4) - 3);
+	
+	return 0;
+}
