80ddf64e3b1b84d33dd843d2f9a91b8ebf362605
Vinicius Teixeira <vinist.002@gmail.com>
2025-06-01 10:46:44 -0300

    Converter para Hexadecimal

---
+++ Todos/C/BEE 1957.c
@@ -0,0 +1,18 @@
+/*
+  autor: Vinist021;
+  data: 03/12/2024;
+  nome: Converter para Hexadecimal;
+*/
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	
+	int n;
+	
+	scanf("%d", &n);
+	
+	printf("%X\n", n);
+	
+	return 0;
+}
