4b2efed87b3d4b9ff59bf2e39c89431db9228599
Vinicius Teixeira <vinist.002@gmail.com>
2025-06-01 10:45:28 -0300

    Tri-du

---
+++ Todos/C/BEE 1933.c
@@ -0,0 +1,25 @@
+/*
+  autor: Vinist021;
+  data: 02/12/2024;
+  nome: Tri-du;
+*/
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	
+	int A, B;
+	
+	scanf("%d %d", &A, &B);
+	
+	if(A > B)
+	{
+		printf("%d\n", A);
+	}
+	else
+	{
+		printf("%d\n", B);
+	}
+	
+	return 0;
+}
