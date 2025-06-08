7b03c6604ea43183a51e63766723c630962594fa
Vinicius Teixeira <vinist.002@gmail.com>
2025-06-08 08:37:10 -0300

    Senha

---
+++ Todos/C/BEE 2146.c
@@ -0,0 +1,20 @@
+/*
+  autor: Vinist021;
+  data: 31/10/2024;
+  nome: Senha; 
+*/
+
+#include <stdio.h>
+
+
+int main(int argc, char *argv[]) {
+	
+	int N;
+
+	while(scanf("%d", &N) != EOF){
+	
+		printf("%d\n", N-1);		
+	}
+	
+	return 0;
+}
