244cf1aef45ed61c126b872108621e60e0959212
Vinicius Teixeira <vinist.002@gmail.com>
2025-06-15 10:00:57 -0300

    Feliz Nataaal!

---
+++ Todos/C/BEE 2483.c
@@ -0,0 +1,23 @@
+/*
+  autor: Vinist021;
+  data: 03/02/2025;
+  nome: Feliz Nataaal!; 
+*/
+
+#include <stdio.h>
+
+int main() {
+	
+	int l;
+	
+	scanf("%d", &l);
+	printf("Feliz nat");
+	
+	for(int i = 0; i < l; i++)
+	{
+		printf("a");
+	}
+	printf("l!\n");
+	
+	return 0;
+}
