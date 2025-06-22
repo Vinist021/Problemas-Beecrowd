b3c12d16899f341328eeda777d3dbdc75e58fdd8
Vinicius Teixeira <vinist.002@gmail.com>
2025-06-22 09:11:57 -0300

    Saída 1

---
+++ Todos/C/BEE 2747.c
@@ -0,0 +1,21 @@
+/*
+  autor: Vinist021;
+  data: 19/08/2024;
+  nome: Saída 1; 
+*/
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	
+	printf("---------------------------------------\n");
+	printf("|                                     |\n");
+	printf("|                                     |\n");
+	printf("|                                     |\n");
+	printf("|                                     |\n");
+	printf("|                                     |\n");
+	printf("---------------------------------------\n");
+	
+	
+	return 0;
+}
