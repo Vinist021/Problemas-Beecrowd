d0614d3796d4d5b4d7cd34088c783b75e1e7eae8
Vinicius Teixeira <vinist.002@gmail.com>
2025-06-22 09:12:59 -0300

    Saída 2

---
+++ Todos/C/BEE 2748.c
@@ -0,0 +1,21 @@
+/*
+  autor: Vinist021;
+  data: 20/08/2024;
+  nome: Saída 2; 
+*/
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	
+	printf("---------------------------------------\n");
+	printf("|        Roberto                      |\n");
+	printf("|                                     |\n");
+	printf("|        5786                         |\n");
+	printf("|                                     |\n");
+	printf("|        UNIFEI                       |\n");
+	printf("---------------------------------------\n");
+	
+	return 0;
+	
+}
