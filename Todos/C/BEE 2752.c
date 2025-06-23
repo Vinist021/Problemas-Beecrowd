fe8cf232ba44c0e8215be17166704ccfd22d2a23
Vinicius Teixeira <vinist.002@gmail.com>
2025-06-23 12:17:08 -0300

    Saída 6

---
+++ Todos/C/BEE 2752.c
@@ -0,0 +1,22 @@
+/*
+  autor: Vinist021;
+  data: 20/08/2024;
+  nome: Saída 6; 
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	
+	printf("<AMO FAZER EXERCICIO NO URI>\n");
+	printf("<    AMO FAZER EXERCICIO NO URI>\n");
+	printf("<AMO FAZER EXERCICIO >\n");
+	printf("<AMO FAZER EXERCICIO NO URI>\n");
+	printf("<AMO FAZER EXERCICIO NO URI    >\n");
+	printf("<AMO FAZER EXERCICIO NO URI>\n");
+	printf("<          AMO FAZER EXERCICIO >\n");
+	printf("<AMO FAZER EXERCICIO           >\n");
+	
+	return 0;
+}
