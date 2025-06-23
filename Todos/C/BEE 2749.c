1153e4ec2e35746debd6d0f8c846010f25412bdf
Vinicius Teixeira <vinist.002@gmail.com>
2025-06-23 12:13:30 -0300

    Saída 3

---
+++ Todos/C/BEE 2749.c
@@ -0,0 +1,20 @@
+/*
+  autor: Vinist021;
+  data: 19/08/2024;
+  nome: Saída 3; 
+*/
+
+#include <stdio.h>
+
+int main() {
+	
+	printf("---------------------------------------\n");
+	printf("|x = 35                               |\n");
+	printf("|                                     |\n");
+	printf("|               x = 35                |\n");
+	printf("|                                     |\n");
+	printf("|                               x = 35|\n");
+	printf("---------------------------------------\n");
+	
+	return 0;
+}
