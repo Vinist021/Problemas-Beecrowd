38b7e5cc9f45b24fc0f011b7fe9a34695fefe572
Vinicius Teixeira <vinist.002@gmail.com>
2025-06-25 18:22:12 -0300

    Saída 9

---
+++ Todos/C/BEE 2755.c
@@ -0,0 +1,17 @@
+/*
+  autor: Vinist021;
+  data: 20/08/2024;
+  nome: Saída 9; 
+*/
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	
+	printf("\"Ro'b'er\tto\\/\"\n");
+	printf("(._.) ( l: ) ( .-. ) ( :l ) (._.)\n");
+    printf("(^_-) (-_-) (-_^)\n");
+    printf("(\"_\") ('.')\n");
+	
+	return 0;
+}
