04e81778f116fa65738c244e05e7fa66eb8fea4b
Vinicius Teixeira <vinist.002@gmail.com>
2025-06-26 12:04:36 -0300

    Saída 10

---
+++ Todos/C/BEE 2756.c
@@ -0,0 +1,22 @@
+/*
+  autor: Vinist021;
+  data: 20/08/2024;
+  nome: Saída 10; 
+*/
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	
+	printf("       A\n");
+	printf("      B B\n");
+	printf("     C   C\n");
+	printf("    D     D\n");
+	printf("   E       E\n");
+	printf("    D     D\n");
+	printf("     C   C\n");
+	printf("      B B\n");
+	printf("       A\n");
+	
+	return 0;
+}
