941e1ed874ffa8e383e25a52feb10f7b0d79c60d
Vinicius Teixeira <vinist.002@gmail.com>
2025-06-30 20:20:27 -0300

    Entrada e Saída 6

---
+++ Todos/C/BEE 2762.c
@@ -0,0 +1,18 @@
+/*
+  autor: Vinist021;
+  data: 24/08/2024;
+  nome: Entrada e Saída 6; 
+*/
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	
+	int A, B;
+	
+	scanf("%d.%d", &A, &B);
+	
+	printf("%d.%d\n", B, A);
+	
+	return 0;
+}
