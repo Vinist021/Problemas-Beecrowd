e8fcefcab4725078fec864a5209a653a7adb51d4
Vinicius Teixeira <vinist.002@gmail.com>
2025-07-13 22:17:49 -0300

    A Saída

---
+++ Todos/C/BEE 2861.c
@@ -0,0 +1,22 @@
+/*
+  autor: Vinist021;
+  data: 26/08/2024;
+  nome: A Saída; 
+*/
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	
+	int C;
+	char pergunta[100];
+	
+	scanf("%d", &C);
+	
+	for(int i=0; i<C; i++){
+		scanf("%s", pergunta);
+		printf("gzuz\n");
+	}
+	
+	return 0;
+}
