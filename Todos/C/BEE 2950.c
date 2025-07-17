0a4502a3d0c85ed759a6e1b608b9c913b68304d2
Vinicius Teixeira <vinist.002@gmail.com>
2025-07-16 21:58:28 -0300

    As Duas Torres

---
+++ Todos/C/BEE 2950.c
@@ -0,0 +1,19 @@
+/*
+  autor: Vinist021;
+  data: 13/02/2025;
+  nome: As Duas Torres; 
+*/
+
+#include <stdio.h>
+
+int main() {
+	
+	float N, X, Y;
+	
+	scanf("%d %d %d", &N, &X, &Y);
+	
+	printf("%.2f\n", N / (X + Y));
+	
+	return 0;
+}
+
