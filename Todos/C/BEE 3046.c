53565be209fd3b76219f7849ed0c8ff5aa88af0a
Vinicius Teixeira <vinist.002@gmail.com>
2025-07-18 21:34:35 -0300

    Dominó

---
+++ Todos/C/BEE 3046.c
@@ -0,0 +1,18 @@
+/*
+  autor: Vinist021;
+  data: 12/02/2024;
+  nome: Dominó; 
+*/
+
+#include <stdio.h>
+
+int main() {
+	
+	int N;
+	
+	scanf("%d", &N);
+	
+	printf("%d\n", ((N+1) * (N+2)) / 2);
+	
+	return 0;
+}
