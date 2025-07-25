17e23de6d1d529f6734ca2ab51e65b100abb19ac
Vinicius Teixeira <vinist.002@gmail.com>
2025-07-24 23:25:22 -0300

    Nota Esquecida

---
+++ Todos/C/BEE 3055.c
@@ -0,0 +1,18 @@
+/*
+  autor: Vinist021;
+  data: 11/02/2025;
+  nome: Nota Esquecida; 
+*/
+
+#include <stdio.h>
+
+int main() {
+	
+	int A, M;
+	
+	scanf("%d %d", &A, &M);
+	
+	printf("%d\n", (2 * M) - A);
+	
+	return 0;
+}
