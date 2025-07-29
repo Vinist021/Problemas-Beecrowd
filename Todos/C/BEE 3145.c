8b81a9229616d268cebb3aec1775e84175de0566
Vinicius Teixeira <vinist.002@gmail.com>
2025-07-29 18:50:36 -0300

    Uma Jornada Inesperada

---
+++ Todos/C/BEE 3145.c
@@ -0,0 +1,18 @@
+/*
+  autor: Vinist021;
+  data: 13/02/2025;
+  nome: Uma Jornada Inesperada; 
+*/
+
+#include <stdio.h>
+
+int main() {
+	
+	float N, X;
+	
+	scanf("%f %f", &N, &X);
+	
+	printf("%.2f\n", X / (N+2));
+	
+	return 0;
+}
