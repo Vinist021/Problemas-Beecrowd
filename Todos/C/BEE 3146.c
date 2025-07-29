ba9629dd0992dc6f16dd40ee3fb7bd457e55ec11
Vinicius Teixeira <vinist.002@gmail.com>
2025-07-29 18:53:02 -0300

    Charadas no Escuro

---
+++ Todos/C/BEE 3146.c
@@ -0,0 +1,18 @@
+/*
+  autor: Vinist021;
+  data: 13/02/2024;
+  nome: Charadas no Escuro; 
+*/
+
+#include <stdio.h>
+
+int main() {
+	
+	float R;
+	
+	scanf("%f", &R);
+	
+	printf("%.2f\n", 2 * 3.14 * R);
+	
+	return 0;
+}
