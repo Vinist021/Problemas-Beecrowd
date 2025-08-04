ebd6f4985e9d7ca83b09f99fd35bad91256278bb
Vinicius Teixeira <vinist.002@gmail.com>
2025-08-03 21:59:59 -0300

    Viva a Ciência!

---
+++ Todos/C/BEE 3157.c
@@ -0,0 +1,18 @@
+/*
+  autor: Vinist021;
+  data: 14/02/2024;
+  nome: Viva a Ciência!; 
+*/
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	
+	char texto[281];
+	
+	scanf("%280[^\n]s", texto);
+	
+	printf("Nao se refuta ciencia com opiniao\n");
+	
+	return 0;
+}
