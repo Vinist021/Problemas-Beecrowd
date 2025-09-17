b5080ee63c05c73c4a7dcddf75b393cc109d84a7
Vinicius Teixeira <vinist.002@gmail.com>
2025-09-16 23:46:59 -0300

    Resposta Certa

---
+++ Todos/Python/BEE 3302.py
@@ -0,0 +1,9 @@
+#   autor: Vinist021;
+#   data: 16/02/2025;
+#   nome: Resposta Certa;
+
+N = int(input())
+
+for i in range(N):
+    resposta = int(input())
+    print(f'resposta {i+1}: {resposta}')
