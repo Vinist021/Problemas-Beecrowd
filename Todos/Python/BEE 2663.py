f3ccbd4a18d697f31b2897ca12d1cabffc4208e2
Vinicius Teixeira <vinist.002@gmail.com>
2025-10-07 18:01:06 -0300

    Fase

---
+++ Todos/Python/BEE 2663.py
@@ -0,0 +1,24 @@
+#   autor: Vinist021;
+#   data: 06/01/2025;
+#   nome: Triângulo;
+
+N = int(input())
+K = int(input())
+L = []
+
+for _ in range(N):
+    L.append(int(input()))
+
+L.sort(reverse=True)
+tam = len(L)
+
+desempate = L[K-1]
+
+i = 0
+classificados = 0
+
+while i < tam and L[i] >= desempate:
+    i += 1
+    classificados += 1
+
+print(classificados)
