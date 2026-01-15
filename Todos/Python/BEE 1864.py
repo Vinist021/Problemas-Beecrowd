032231089ea20ba823cf8a8110529c879bb671dd
Vinicius Teixeira <viniciusds.teixeira03@gmail.com>
2026-01-14 21:51:39 -0300

    Nossos Dias Nunca Voltarão

---
+++ Todos/Python/BEE 1864.py
@@ -0,0 +1,10 @@
+#   autor: Vinist021;
+#   data: 15/12/2024;
+#   nome: Nossos Dias Nunca Voltarão;
+
+frase = 'LIFE IS NOT A PROBLEM TO BE SOLVED'
+N = int(input())
+
+for i in range(N):
+    print(frase[i], end='')
+print()
